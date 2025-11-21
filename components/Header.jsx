11079a1b216dbad109ac0aadd8e80f1648a9b444
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-11-21 18:20:00 +0200

    up

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,17 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 export default function Header() {
   return (
-    <div dir="ltr" className={` h-20 w-full bg-[#000000d7] text-white
-    flex justify-between items-center p-5`}>
-     
-  <div
+    <div
+      dir="ltr"
+      className={` h-20 w-full bg-[#000000d7] text-white
+    flex justify-between items-center p-5`}
+    >
+      <Link href={"/"}>
+        <div
           className={`logo w-[70px] h-[70px] relative overflow-hidden 
              rounded-full   flex justify-center items-center shrink-0
-            bg-white border-4 border-black/55 
+            bg-white
              `}
         >
           <Image
@@ -16,16 +19,14 @@ export default function Header() {
             alt="my pic"
             width={70}
             height={70}
-           className="mt-12"
+            className=""
           />
-    
-        
-      </div>
+        </div>
+      </Link>
       <div className={`links flex items-center gap-5 text-lg  md:text-xl`}>
         <Link href={``}>عني</Link>
         <Link href={``}>سابقة أعمالي</Link>
       </div>
-     
     </div>
-  )
+  );
 }
